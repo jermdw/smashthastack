c7ca482a88703239fdfcc44ce01525ed7b6c232e
jermdw@gmail.com <jermdw@gmail.com>
2017-08-13 12:42:16 -0400

    app fuzzing ftw

---
+++ format-string/format-string.c
@@ -0,0 +1,11 @@
+//format string example
+//h0neybadger 20170813
+#include <stdio.h>
+
+void main(int argc, char** argv)
+{
+	// printf accepts mutiple arguments
+	// first argument is a format string
+	// many programmers assume this is only argument
+	printf(argv[1]);
+}
